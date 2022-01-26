af1e4f0cdd43c0cc3fbd19801a25216f76498558
tarek abdelhak <tarekroja123@gmail.com>
2022-01-26 16:27:49 +0200

    create users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string("full_name");
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->boolean("is_admin");
+            $table->boolean("is_trusted");
+            $table->boolean("is_registered");
         });
     }
 
