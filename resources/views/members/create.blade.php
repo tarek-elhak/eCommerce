490963dc05461e2d4eb1d6d75516193180e882a2
tarek abdelhak <tarekroja123@gmail.com>
2022-02-04 17:03:30 +0200

    add TODO for functioning Eye icon on create member form

--- resources/views/members/create.blade.php
+++ resources/views/members/create.blade.php
@@ -76,6 +76,10 @@
                     <div class="mb-6">
                         <div class="w-full flex relative items-center">
                             <input class="outline-0 p-2 border focus:border-indigo-900 flex-1 rounded" type="password" name="password" id="password" placeholder="password" />
+                            <!--
+                                -- TODO
+                                -- Eye Icon not working here
+                            -->
                             <i class="fa fa-eye absolute right-2 cursor-pointer"></i>
                         </div>
                         @error("password")
