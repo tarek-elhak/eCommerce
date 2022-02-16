1b3d3c57521f1700e81d5b1342e3a0bec7efd58b
= <=>
2022-02-17 01:55:41 +0200

    make the App  prevents the lazy loading for relationships

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // make the app notify me whenever a lazy loading for relationships is happening
+        Model::preventLazyLoading();
     }
 }
