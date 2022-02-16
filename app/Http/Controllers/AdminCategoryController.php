91b4be013b7926912c9cb70b3b2f022766505e23
= <=>
2022-02-16 15:49:00 +0200

    validate the uniqueness of the category name

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Validation\Rule;
 
 class AdminCategoryController extends Controller
 {
@@ -15,7 +16,7 @@ class AdminCategoryController extends Controller
     public function store(Request $request)
     {
         $attributes = $request->validate([
-           'category_name' => ["required"]
+           'category_name' => ["required" , Rule::unique("categories","category_name")]
         ]);
 
         $category = Category::create($attributes);
