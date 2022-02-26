eb1e616d8d374cf6648407ce814fae7629b3eb8c
= <=>
2022-02-26 10:52:58 +0200

    add items table,model and controller

---
+++ database/migrations/2022_02_26_082411_create_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("member_id")->constrained("users")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId("category_id")->constrained("categories")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string("name");
+            $table->text("description");
+            $table->unsignedFloat("price");
+            $table->string("currency",3);
+            $table->string("made_country");
+            $table->string("image");
+            $table->unsignedSmallInteger("rating");
+            $table->string("status");
+            $table->unsignedSmallInteger("number_of_available_pieces");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
