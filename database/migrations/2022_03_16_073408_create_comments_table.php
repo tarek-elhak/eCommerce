0299c7590d037a766fe8e30c8a1f82eebe3f7b5e
= <=>
2022-03-16 11:07:42 +0200

    create comments table

---
+++ database/migrations/2022_03_16_073408_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text("body");
+            $table->boolean("is_approved");
+            $table->foreignId("item_id")->constrained("items")->cascadeOnUpdate()->cascadeOnUpdate();
+            $table->foreignId("member_id")->constrained("users")->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
